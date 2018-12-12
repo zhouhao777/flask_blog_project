483748371996b62c964fadc215133aadd6df20d7
zhouhao <zhouhao777@126.com>
2018-12-12 23:14:35 +0800

    add category

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, request, Blueprint
-from flaskblog.models import Post
+from flask import render_template, request, Blueprint, abort
+from flaskblog.models import Post,User
 
 main = Blueprint('main', __name__)
 
@@ -15,6 +15,17 @@ def home():
 @main.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+
+@main.route("/category/<category_name>")
+def get_category_post(category_name):
+    category_list = ['daily','test','tech']
+    if category_name not in category_list:
+        abort(404)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.filter_by(category=category_name).order_by(Post.date_posted.desc()).paginate(page=page, per_page=6)
+    return render_template('home.html', title=category_name, posts=posts)
 
 
 
