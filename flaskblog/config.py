a9827000621ba55e5ee1397cb7884e131b23f92d
周浩 <zhouhao@mafengwo.com>
2018-12-12 17:19:37 +0800

    update

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,8 +2,7 @@ import os
 
 
 class Config:
-    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:ceshi123@127.0.0.1:3306/flask?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://'+ os.environ.get('DATABASE_USER') + ':'+ os.environ.get('DATABASE_PASSWORD') + '@127.0.0.1:3306/flask?charset=utf8'
 
